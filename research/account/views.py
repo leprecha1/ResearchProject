c31a69d853fd814191a38bd2a3b57fe82433d625
Rafael Lucas <rafalucas.unicamp@gmail.com>
2012-10-07 21:35:57 -0300

    Adding expiration date verification.

--- research/account/views.py
+++ research/account/views.py
@@ -42,39 +42,39 @@ class dissertativeForm(ModelForm):
 
 def home(request):
     if request.user:
-	user = request.user 
+        user = request.user
     else:
-	user = "AnonymousUser"
-    
+        user = "AnonymousUser"
+
     return render("index.html", {
-	"user": user,
+        "user": user,
     }, context_instance=RequestContext(request))
 
 def loginPage(request, cookie):
     if request.POST:
         frm = loginForm(request.POST)
         frm_dict = request.POST
-	user_email = frm_dict['username']
+        user_email = frm_dict['username']
         username = get_object_or_404(User, email=user_email)
         user = authenticate(username=username, password=frm_dict['passwd'])
-	
-	if user is not None:
-	    if user.is_active and user.is_staff:
-	        login(request, user)
-	    	return HttpResponseRedirect('/admin/')
-	    elif user.is_active:
-		login(request, user)
-		return HttpResponseRedirect('/home/%s'%user)
-	    else:
-		return HttpResponseRedirect('/')
-	else:
-	    return HttpResponseRedirect('/')
+
+        if user is not None:
+            if user.is_active and user.is_staff:
+                login(request, user)
+                return HttpResponseRedirect('/admin/')
+            elif user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/home/%s'%user)
+            else:
+                return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/')
     else:
         frm = loginForm()
 
-    	return render("login.html", {
-       	    "frm": frm,
-    	}, context_instance=RequestContext(request))
+        return render("login.html", {
+            "frm": frm,
+        }, context_instance=RequestContext(request))
 
 def signup(request, cookie):
     if request.POST:
@@ -92,9 +92,9 @@ def signup(request, cookie):
     else:
         frm = signupForm()
 
-    	return render("signup.html", {
-       	    "frm": frm,
-    	}, context_instance=RequestContext(request))
+        return render("signup.html", {
+            "frm": frm,
+        }, context_instance=RequestContext(request))
 
 @user_passes_test(lambda u: u.is_staff, login_url='/login/')
 def admin(request, cookie):
@@ -102,13 +102,13 @@ def admin(request, cookie):
         frm = loginForm(request.POST)
         if frm.is_valid():
             frm_dict = request.POST
-    else:
-        frm = loginForm()
+        else:
+            frm = loginForm()
 
-    	return render("admin.html", {
-        	"frm": frm,
+        return render("admin.html", {
+            "frm": frm,
             "user": request.user,
-    	}, context_instance=RequestContext(request))
+        }, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 @user_passes_test(lambda u: u.is_staff, login_url='/login/')
@@ -118,55 +118,54 @@ def research_edit(request, cookie):
     if request.POST:
         frm_dict = request.POST
         edit_resear = research.objects.filter(id=research_id).update(title = frm_dict['title'],
-						       description = frm_dict['description'],
-   						       start_at = frm_dict['start_at'],
-						       finish_at = frm_dict['finish_at'],
-						       status = frm_dict['status'],
-						       publish = frm_dict['publish'])
+                                                                     description = frm_dict['description'],
+                                                                     start_at = frm_dict['start_at'],
+                                                                     finish_at = frm_dict['finish_at'],
+                                                                     status = frm_dict['status'],
+                                                                     publish = frm_dict['publish'])
         return HttpResponseRedirect('/research')
     else:
-	
-	edit_resear = research.objects.get(id=research_id)
-	frm = researchForm(instance=edit_resear)
+        edit_resear = research.objects.get(id=research_id)
+        frm = researchForm(instance=edit_resear)
 
-    return render("edit_research.html", {
-		"research": research.objects.get(id=research_id),
-		"frm": frm,
-    }, context_instance=RequestContext(request))
+        return render("edit_research.html", {
+            "research": research.objects.get(id=research_id),
+            "frm": frm,
+        }, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 @user_passes_test(lambda u: u.is_staff, login_url='/login/')
 def research_delete(request):
-	research_id = re.sub(r"\/research\/",'', request.path)
-	research_id = re.sub(r"\/delete",'', research_id)
-	research.objects.get(id=research_id).delete()
-	return HttpResponseRedirect('/research')
+    research_id = re.sub(r"\/research\/",'', request.path)
+    research_id = re.sub(r"\/delete",'', research_id)
+    research.objects.get(id=research_id).delete()
+    return HttpResponseRedirect('/research')
 
 @login_required(login_url='/login/')
 @user_passes_test(lambda u: u.is_staff, login_url='/login/')
 def new_question(request):
-	research_id = re.sub(r"\/research\/",'', request.path)
-	research_id = re.sub(r"\/question\/new",'', research_id)
+    research_id = re.sub(r"\/research\/",'', request.path)
+    research_id = re.sub(r"\/question\/new",'', research_id)
 
-	if request.POST:
-        	frm_diss = dissertativeForm(request.POST)
-		frm_mult = multForm(request.POST) 
-	else:
-        	frm_diss = dissertativeForm()
-		frm_mult = multForm() 
-		
-   	return render("add_question.html", {
-    		"research": research.objects.get(id=research_id),
-		"multForm": frm_mult,
-		"dissForm": frm_diss,
+    if request.POST:
+        frm_diss = dissertativeForm(request.POST)
+        frm_mult = multForm(request.POST)
+    else:
+        frm_diss = dissertativeForm()
+        frm_mult = multForm()
+
+    return render("add_question.html", {
+        "research": research.objects.get(id=research_id),
+        "multForm": frm_mult,
+        "dissForm": frm_diss,
     }, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def research_answer(request):
-	research_id = int(re.sub(r"\/research\/",'', request.path))
-    	return render("answer_research.html", {
-		"research": research.objects.get(id=research_id)
-    	}, context_instance=RequestContext(request))
+    research_id = int(re.sub(r"\/research\/",'', request.path))
+    return render("answer_research.html", {
+        "research": research.objects.get(id=research_id)
+    }, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 @user_passes_test(lambda u: u.is_staff, login_url='/login/')
@@ -179,33 +178,38 @@ def research_new(request):
                               start_at = frm_dict['start_at'],
                               finish_at = frm_dict['finish_at'],
                               status = frm_dict['status'],
-			      publish = frm_dict['publish'])
+                              publish = frm_dict['publish'])
         new_resear.save()
         return HttpResponseRedirect('/research')
     else:
         frm = researchForm()
 
-    	return render("new_research.html", {
+        return render("new_research.html", {
             "frm": frm,
             "user": request.user,
-	    "list_research": lst_research,
-    	}, context_instance=RequestContext(request))
+            "list_research": lst_research,
+        }, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def research_list(request):
-   
-    now = datetime.datetime.now() 
-    print datetime.datetime.now().strftime("%b. %d,%Y") 
+
+    expiration_list = []
+    now =  datetime.datetime.now().date()
+
+    for i in research.objects.all():
+        if i.finish_at < now:
+            expiration_list.append(i.id)
 
     return render("research_list.html", {
-            "user": request.user,
-            "list_research": research.objects.all(),
+        "user": request.user,
+        "expiration_list": expiration_list,
+        "list_research": research.objects.all(),
     },context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def userhome(request, cookie):
     return render("home.html", {
-            "user": request.user,
+        "user": request.user,
     },context_instance=RequestContext(request))
 
 def logoff(request, cookie):
